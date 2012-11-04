7d0b0084e4fc0244a505a41ca4113159745e81d4
adorsk <adorsk>
2012-11-04 11:06:41 -0500

    simplified schema to just use sequences as ids

--- lib/proteomics_dao/schema.py
+++ lib/proteomics_dao/schema.py
@@ -4,19 +4,23 @@ The goal of this schema is to make proteomics data ingesta and analysis
 modular and flexible.
 """
 
+from sqlalchemy import (MetaData, Table, Column, Integer, String, ForeignKey)
+
+
+# Initialize Metadata.
+metadata = MetaData()
+
 # Define tables.
 tables = {}
 
 tables['proteins'] = Table(
     'proteins', metadata,
-    Column('id', String, primary_key=True),
-    Column('sequence', String)
+    Column('sequence', String, primary_key=True)
 )
 
 tables['peptides'] = Table(
     'peptides', metadata,
-    Column('id', String, primary_key=True),
-    Column('sequence', String)
+    Column('sequence', String, primary_key=True)
 )
 
 tables['genomes'] = Table(
@@ -33,31 +37,18 @@ tables['digests'] = Table(
 Here comes the Greek...
 A 'Proteolysis' is the breakdown of a protein into sub-entities.
 These sub-entities are typically peptides.
-We represent a proteolysis record as a
-protein-digest pair.
-"""
-tables['proteolyses'] = Table(
-    'proteolyses', metadata,
-    Column('protein_id', String, 
-           ForeignKey('proteins.id'), primary_key=True),
-    Column('digest_id', String, 
-           ForeignKey('digests.id'), primary_key=True),
-)
-
-"""
-A proteolysis_product represents a sub-entity produced by
-a proteolysis. This is typically a peptide.
+We represent a proteolysis_product as a
+protein-digest-peptide triple.
 """
 tables['proteolysis_products'] = Table(
+    'proteolysis_products', metadata,
     # Composite foreign-key for proteolyses.
-    Column('protein_id', String, nullable=False, primary_key=True),
-    Column('digest_id', String, nullable=False, primary_key=True),
-    ForeignKeyConstraint(
-        ['protein_id', 'digest_id'], 
-        ['proteolyses.protein_id', 'proteolyses.digest_id']
-    ),
-    Column('peptide_id', String, 
-           ForeignKey('peptides.id'), primary_key=True),
+    Column('protein_sequence', String, 
+           ForeignKey('proteins.sequence'), primary_key=True),
+    Column('digest_id', String,
+           ForeignKey('digests.id'), primary_key=True),
+    Column('peptide_sequence', String, 
+           ForeignKey('peptides.sequence'), primary_key=True),
 )
 
 """
@@ -67,5 +58,15 @@ is a genome.
 We use protein records because the same protein can appear multiple
 times w/in a genome.
 """
-#@TODO change this to genomes_proteins?
-tables['protein_instances'] = None
+tables['protein_instances'] = Table(
+    'protein_instances', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('protein_sequence', String, 
+           ForeignKey('proteins.sequence')),
+    Column('genome_id', String, 
+           ForeignKey('genomes.id')),
+    # Hang on to metadata about the instance.
+    # We may use this later to reconcile proteins
+    # with external databases.
+    Column('metadata', String),
+)
